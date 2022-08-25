93a685e67ba962642ec2d08b24b30ebf6dd495d5
slorello <steve.lorello@redislabs.com>
2022-08-25 13:47:19 -0400

    switching to simplified redis image based version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-./src/redis-server tls.conf
+redis-server tls.conf
 # ./src/redis-server --tls-port 6379 --port 0 --tls-cert-file /redis/certs/redis.crt --tls-key-file /redis/certs/redis.key --tls-ca-cert-file /redis/certs/ca.crt --protected-mode no --tls-auth-clients no --tls-ciphers AES256-SHA256
